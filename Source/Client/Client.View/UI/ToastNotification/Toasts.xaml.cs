0f9649acacbdaf1a419f42b107d380374ea0c3b9
Ed Allonby <edward.allonby@gmail.com>
2015-06-03 12:41:00 +0100

    Reduced size of toasts.

--- Source/Client/Client.View/UI/ToastNotification/Toasts.xaml.cs
+++ Source/Client/Client.View/UI/ToastNotification/Toasts.xaml.cs
@@ -19,6 +19,7 @@ namespace Client.View.UI.ToastNotification
             this.notificationManager = notificationManager;
   
             InitializeComponent();
+            
             NotificationsControl.DataContext = notifications;
         }
 
