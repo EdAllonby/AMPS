e6d3dc496db70c4ea549792119aeb98f6fed88b9
Ed Allonby <edward.allonby@gmail.com>
2015-06-16 13:45:58 +0100

    Added burndown chart with date.

--- Source/Client/Client.ViewModel/SettingsViewModel/BurnDownViewModel.cs
+++ Source/Client/Client.ViewModel/SettingsViewModel/BurnDownViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Linq;
 using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 using Shared;
 using Shared.Domain;
 using Shared.Repository;
@@ -12,7 +14,7 @@ namespace Client.ViewModel.SettingsViewModel
         private readonly Band band;
         private readonly JamRepository jamRepository;
         private readonly TaskRepository taskRepository;
-        private IList<DataPoint> points;
+        private PlotModel plotModel;
 
         public BurnDownViewModel(Band band, IServiceRegistry serviceRegistry) : base(serviceRegistry)
         {
@@ -23,45 +25,125 @@ namespace Client.ViewModel.SettingsViewModel
             taskRepository.EntityAdded += JamUpdated;
             taskRepository.EntityUpdated += JamUpdated;
 
-            UpdatePoints();
+            PlotModel = new PlotModel {Title = "Jam Burndown"};
+
+            UpdateModel();
         }
 
-        public IList<DataPoint> Points
+        public PlotModel PlotModel
         {
-            get { return points; }
+            get { return plotModel; }
             set
             {
-                points = value;
+                plotModel = value;
                 OnPropertyChanged();
             }
         }
 
-        private void UpdatePoints()
+        private Series CreateActualBurndown()
         {
+            Jam jam = jamRepository.GetCurrentActiveJamInBand(band.Id);
+
+            var actualBurndown = new LineSeries
+            {
+                Title = "Actual Burndown",
+                StrokeThickness = 2,
+                MarkerSize = 1,
+                MarkerStroke = OxyColors.DarkRed,
+                MarkerType = MarkerType.Diamond,
+                CanTrackerInterpolatePoints = false
+            };
+
+            if (jam != null)
+            {
+                int totalPoints = taskRepository.GetTasksInJam(jam.Id).Sum(x => x.Points);
+
+                int days = 0;
+
+                actualBurndown.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Now.AddDays(days)), totalPoints));
+
+                foreach (Task task in taskRepository.GetTasksInJam(jam.Id).ToList())
+                {
+                    totalPoints -= task.Points;
+                    days++;
+
+                    actualBurndown.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Now.AddDays(days)), totalPoints));
+                }
+            }
+
+            return actualBurndown;
+        }
+
+        private Series CreateExpectedBurndown()
+        {
+            var projectedBurndownSeries = new LineSeries
+            {
+                Title = "Projected Burndown",
+                StrokeThickness = 2,
+                MarkerSize = 1,
+                MarkerStroke = OxyColors.Coral,
+                MarkerType = MarkerType.Star,
+                CanTrackerInterpolatePoints = false
+            };
+
             Jam jam = jamRepository.GetCurrentActiveJamInBand(band.Id);
 
             if (jam != null)
             {
                 int totalPoints = taskRepository.GetTasksInJam(jam.Id).Sum(x => x.Points);
-                int startingPosition = 0;
 
-                Points = new List<DataPoint>();
+                var daysAhead = 0;
 
-                Points.Add(new DataPoint(startingPosition, totalPoints));
+                projectedBurndownSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Now.AddDays(daysAhead)), totalPoints));
 
-                foreach (Task task in taskRepository.GetTasksInJam(jam.Id).ToList())
+                const int projectedPointsPerDay = 7;
+
+                while (totalPoints > 0)
                 {
-                    startingPosition += 10;
-                    totalPoints -= task.Points;
+                    daysAhead++;
 
-                    Points.Add(new DataPoint(startingPosition, totalPoints));
+                    DateTime nextDate = DateTime.Now.AddDays(daysAhead);
+
+                    if (IsWorkingDay(nextDate))
+                    {
+                        if (totalPoints <= projectedPointsPerDay)
+                        {
+                            totalPoints = 0;
+                        }
+                        else
+                        {
+                          totalPoints -= projectedPointsPerDay;                            
+                        }
+                    }
+
+                    projectedBurndownSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(nextDate), totalPoints));
                 }
             }
+
+            return projectedBurndownSeries;
+        }
+
+        private static bool IsWorkingDay(DateTime nextDate)
+        {
+            return nextDate.DayOfWeek != DayOfWeek.Saturday && nextDate.DayOfWeek != DayOfWeek.Sunday;
+        }
+
+        private void UpdateModel()
+        {
+            PlotModel = new PlotModel {Title = "Jam Burndown"};
+
+            var dateAxis = new DateTimeAxis {Title = "Projected Finish"};
+            PlotModel.Axes.Add(dateAxis);
+            var valueAxis = new LinearAxis {MajorGridlineStyle = LineStyle.Solid, MinorGridlineStyle = LineStyle.Dot, Title = "Value"};
+            PlotModel.Axes.Add(valueAxis);
+
+            PlotModel.Series.Add(CreateExpectedBurndown());
+            PlotModel.Series.Add(CreateActualBurndown());
         }
 
         private void JamUpdated(object sender, EntityChangedEventArgs<Task> e)
         {
-            UpdatePoints();
+            UpdateModel();
         }
     }
 }
